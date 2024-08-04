c2a10fb4022b2278f2270610362540b1183ba240
PsaTyrE <113742594+PsaTyrE@users.noreply.github.com>
2024-08-04 08:06:36 -0700

    chore: add table and fillable

--- app/Models/Departemen.php
+++ app/Models/Departemen.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Departemen extends Model
 {
     use HasFactory;
-
-    protected $fillable = [];
+    protected $table = 'departemen';
+    protected $fillable = ['nama_departemen'];
 
     public function issue()
     {
